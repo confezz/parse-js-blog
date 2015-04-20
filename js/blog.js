6ff48bf2e5f62c5fbaf60151bc49ca09d04e0342
Confezz <confezz.app@gmail.com>
2015-04-20 15:57:31 +0530

    Updated parse creds

--- js/blog.js
+++ js/blog.js
@@ -3,8 +3,8 @@ $(function() {
 	Parse.$ = jQuery;
 
 	// Replace this line with the one on your Quickstart Guide Page
-	// Parse.initialize("rP8DJxprnvYyHZv0eoEqeyDDxavOdculegUVpdw3", "Rvd9F4i0S4AcQS31qmOzo0EwbR8dYWxCOPDjGYG2");
-	Parse.initialize("HC87tn6aA7c3sYx9X0vwwLVXeqHDRMYYmrUBK5zv", "3piNGGnRMhvWo8u9pKD9TDc1MJlWhlvK78Vr3fHo");
+	Parse.initialize("rP8DJxprnvYyHZv0eoEqeyDDxavOdculegUVpdw3", "Rvd9F4i0S4AcQS31qmOzo0EwbR8dYWxCOPDjGYG2");
+	// Parse.initialize("HC87tn6aA7c3sYx9X0vwwLVXeqHDRMYYmrUBK5zv", "3piNGGnRMhvWo8u9pKD9TDc1MJlWhlvK78Vr3fHo");
 
 	var Blog = Parse.Object.extend("Blog"),
 		Blogs = Parse.Collection.extend({
